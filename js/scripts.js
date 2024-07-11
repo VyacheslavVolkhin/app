ad82164ea2d084853f57ae0c133b9c46055f006c
VyacheslavVolkhin <vv@htmlworker.com>
2024-07-11 06:51:27 +0500

    add

--- js/scripts.js
+++ js/scripts.js
@@ -272,48 +272,50 @@ popupElements.forEach((element) => {
   }
 });
 
+
 // Popups
 let popupCurrent;
+let popupsList = document.querySelectorAll('.popup-outer-box')
 
 document.querySelectorAll(".js-popup-open").forEach(function (element) {
   element.addEventListener("click", function (e) {
-    document.querySelector(".popup-outer-box").classList.remove("active");
-    document.body.classList.add("popup-open");
+	document.querySelector(".popup-outer-box").classList.remove("active");
+	document.body.classList.add("popup-open");
 
-    popupCurrent = this.getAttribute("data-popup");
-    document
-      .querySelector(
-        `.popup-outer-box[id="${popupCurrent}"
+	popupCurrent = this.getAttribute("data-popup");
+	document
+	  .querySelector(
+		`.popup-outer-box[id="${popupCurrent}"
 		]`
-      )
-      .classList.add("active");
+	  )
+	  .classList.add("active");
 
-    e.preventDefault();
-    e.stopPropagation();
-    return false;
-  });
+	e.preventDefault();
+	e.stopPropagation();
+	return false;
+	});
 });
 document.querySelectorAll(".js-popup-close").forEach(function (element) {
-  element.addEventListener("click", function (e) {
-    document.body.classList.remove("popup-open");
-    document.querySelector(".popup-outer-box").classList.remove("active");
-
-    e.preventDefault();
-    e.stopPropagation();
-    return false;
-  });
+  element.addEventListener("click", function (event) {
+	document.body.classList.remove("popup-open");
+	for (i=0;i<popupsList.length;i++) {
+		popupsList[i
+			].classList.remove("active");
+		}
+	event.stopPropagation();
+	});
 });
 document.querySelectorAll(".popup-outer-box").forEach(function (element) {
   element.addEventListener("click", function (event) {
-    if (!event.target.closest(".popup-box")) {
-      document.body.classList.remove("popup-open");
-      document.body.classList.remove("popup-open-scroll");
-      document.querySelectorAll(".popup-outer-box").forEach(function (e) {
-        e.classList.remove("active");
-      });
-      return false;
-    }
-  });
+	if (!event.target.closest(".popup-box")) {
+	  document.body.classList.remove("popup-open");
+	  document.body.classList.remove("popup-open-scroll");
+	  document.querySelectorAll(".popup-outer-box").forEach(function (e) {
+		e.classList.remove("active");
+			});
+	  return false;
+		}
+	});
 });
 
 
@@ -444,10 +446,7 @@ const swiperSliderPhotos = new Swiper(".slider-photos .swiper", {
       slidesPerView: 2,
     },
     1024: {
-      slidesPerView: 3,
-      grid: {
-        rows: 2,
-      },
+      slidesPerView: 'auto',
     },
   },
 });
